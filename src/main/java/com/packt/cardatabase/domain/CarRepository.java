54b62989a946cdbd2fc093b5696ef71186a97f58
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-06-23 21:13:29 -0500

    Use the OrderBy keyword
    Queries can be sorted by using the OrderBy keyword in the query method.

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -19,4 +19,7 @@ public interface CarRepository extends CrudRepository<Car, Long> {
 
     // Fetch cars by brand or color
     List<Car> findByBrandOrColor(String brand, String color);
+
+    // Fetch cars by brand and sort by year
+    List<Car> findByBrandOrderByYearAsc(String brand);
 }
