88ae6638f7b0528306996c20671889dad41e783e
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-06-27 13:04:15 -0500

    Use spring data REST
    We add the Spring Data REST  dependency to the pom.xml file. By default,
    Spring Data REST finds all public repositories from the application and
    creates RESTful web services for your entities automatically.
    
    You can define the endpoint of service in your application.properties
    files.
    
    spring.data.rest.basePath=/api
    
    We can see that there are links to the car and owner entity services.
    The Spring Data REST service path name is derived from the entity name.
    The name will then be pluralized and uncapitalized. For example, the
    entity Car service path name will be named cars. The profile link is
    generated by Spring Data REST and contains application-specific
    metadata.

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,12 +2,18 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
+    // Fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
     // Fetch cars by color
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color") String color);
 
     // Fetch cars by year
     List<Car> findByYear(int year);
@@ -20,10 +26,6 @@ public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
 
     // Fetch cars by brand and sort by year
     List<Car> findByBrandOrderByYearAsc(String brand);
-
-    // Fetch cars by brand using SQL
-    @Query("select c from Car c where c.brand = ?1")
-    List<Car> findByBrand(String brand);
 
     // Fetch cars by brand using SQL
     @Query("select c from Car c where c.brand like %?1")
