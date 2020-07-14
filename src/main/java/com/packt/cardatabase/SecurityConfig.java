9b6707509dceb00036ff71774be9a2c1b36775e7
Arnold Martinez <arnoldmartinez@criptext.com>
2020-07-13 22:12:14 -0500

    Change the SecurityConfig class's configure method
    We stipulate that the POST method request to the /login endpoint is
    allowed without authentication and that request to all other endpoints
    require authentication. We also define the filters to be used in the
    /login and other endpoints by using the addFilterBefore method.

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.packt.cardatabase;
 import com.packt.cardatabase.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // Filter for the api/login requests
+                .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // Filter for other requests to check JWT in header
+                .addFilterBefore(new AuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 }
